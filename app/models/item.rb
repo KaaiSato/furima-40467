83c8899045dac96582d116451ca3bf8c2b4be49e
KaaiSato <kaai.sato@gmail.com>
2024-07-11 15:53:32 +0900

    商品出品機能の修正①

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A\d+\z/, message: '半角数字を使用して下さい' }
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: '半角数字を使用して下さい' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: '販売可能価格外です' }
+  validates :user_id, presence: true
 end
